c811f5767ae2e35c3992de0e7448e5d9b917b7e6
Sam Stine <samstine89@gmail.com>
2017-07-11 19:05:30 -0500

    Filtered LS with count output

---
+++ solutions/filteredls.js
@@ -0,0 +1,37 @@
+var fs = require("fs");
+var path = require("path");
+
+var directroyPath = process.argv[2];
+var fileExtension = process.argv[3];
+
+if (!fileExtension.startsWith("."))
+{
+    fileExtension = "." + fileExtension;
+}
+
+if (!directroyPath.endsWith("/"))
+{
+    directroyPath += "/";
+}
+
+function readDirectoryAsync() {
+    fs.readdir(directroyPath, function doneReading(err, directoryContents){
+        if (err !== null) {
+            return console.log(err);
+        }
+
+        var matchingFileCount = 0;
+        directoryContents.forEach(function(fileName) {
+            var currentFileExtension = path.extname(fileName);
+
+            if (currentFileExtension === fileExtension)
+            {
+                matchingFileCount++;
+            }
+        });
+
+        console.log(matchingFileCount);
+    });
+}
+
+readDirectoryAsync();
