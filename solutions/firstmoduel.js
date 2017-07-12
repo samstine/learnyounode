f6d7d5b22849ef1d1b3e061c4cbc1917bef5fdef
Sam Stine <samstine89@gmail.com>
2017-07-11 19:47:56 -0500

    First pass at first module

---
+++ solutions/firstmoduel.js
@@ -0,0 +1,41 @@
+var fs = require("fs");
+var path = require("path");
+
+function getFilesWithExtenstion(directoryName, fileExtension, printFileNames)
+{
+    if (!fileExtension.startsWith("."))
+    {
+        fileExtension = "." + fileExtension;
+    }
+
+    fs.readdir(directoryName, function doneReading(err, allFileNames) {
+        if (err !== null) {
+            return printFileNames(err);
+        }
+        
+        var filteredFileNames = [];
+
+        allFileNames.forEach(function(fileName){
+            var currentFileExtension = path.extname(fileName);
+
+            if (currentFileExtension === fileExtension)
+            {
+                filteredFileNames.add(fileName);
+            }
+        });
+
+        return printFileNames(null, filteredFileNames);
+    });
+}
+
+function printFileNames(err, fileNames)
+{
+    if (err !== null)
+    {
+        console.log(err);
+    } else {
+        fileNames.each(function(fileName){
+            console.log(fileName);
+        });
+    }
+}
